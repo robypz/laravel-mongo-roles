577b2c6ce0331701aad3110773dfe04766036377
Robert Yepez <robertyepez0208@hotmail.com>
2025-07-21 17:08:14 +0200

    fix: correct namespace for UserFactory in DatabaseSeeder and add mongorole configuration file

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Workbench\Database\Factories\UserFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Workbench\Workbench\Database\Factories\UserFactory;
 
 class DatabaseSeeder extends Seeder
 {
