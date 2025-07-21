0f176dd91a861ffd0a82076afd18fd37c424f211
Robert Yepez <robertyepez0208@hotmail.com>
2025-07-21 16:51:08 +0200

    fix: update package name and namespace for MongoRoleServiceProvider

---
+++ src/providers/MongoRoleServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Robypz\LaravelMongoRoles\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MongoRoleServiceProvider extends ServiceProvider
+{
+    public function register() {}
+
+    public function boot() {}
+}
