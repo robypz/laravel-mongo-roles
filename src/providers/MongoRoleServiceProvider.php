a7e0074181f58a2d08f45613dd42d43946e0a980
Robert Yepez <robertyepez0208@hotmail.com>
2025-07-28 15:34:03 +0200

    fix: correct path for publishing migrations in MongoRoleServiceProvider

--- src/providers/MongoRoleServiceProvider.php
+++ src/providers/MongoRoleServiceProvider.php
@@ -20,7 +20,7 @@ class MongoRoleServiceProvider extends ServiceProvider
     {
         //Publish Migrations
         $this->publishesMigrations([
-            __DIR__ . '/../database/migrations' => database_path('migrations'),
+            __DIR__ . '../database/migrations/' => database_path('migrations'),
         ]);
 
         //Migrations
